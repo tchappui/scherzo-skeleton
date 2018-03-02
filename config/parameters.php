4fcb562f669a140015d01d9fa6378dadeee2a765
Thierry Chappuis <thierry@chappuis.io>
2018-03-02 12:54:14 +0100

    Added a generic parameters.php file in config.

---
+++ config/parameters.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the scherzo project.
+ *
+ * (c) Thierry Chappuis <thierry@chappuis.io>
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that is distributed at the root of the source
+ * repository.
+ */
+
+return [
+    //Database config
+    'database.host' => 'localhost',
+    'database.user' =>  'skeleton',
+    'database.password' => 'scherzoPass.1977',
+    'database.name' => 'skeleton',
+    'database.charset' => 'utf8mb4'
+    // database used by the unit tests
+    'testing.database.name' => 'testskeleton',
+    'testing.database.charset' => 'utf8mb4'
+];
+
